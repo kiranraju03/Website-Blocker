99214c0088a2f74df08f332ad812f6a106d6f7e8
kiranraju03 <kiran.raju.k.43@gmail.com>
2017-02-14 20:19:02 +0530

    Writing into the hosts file

--- source.py
+++ source.py
@@ -8,7 +8,15 @@ website_list=["www.python.org","python.org"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day, 20)< dt.now() < dt(dt.now().year,dt.now().month,dt.now().day, 21):
-        print("dont open")
+        print("Should not access the website")
+        with open(host_file,"r+") as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+"   "+website+"\n")
+                    
     else:
         print("can open links")
-time.sleep(5)    
+time.sleep(5)
